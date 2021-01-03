5e28ad008e82fbabfc525fbf5c3a0a8f99e9fa47
emilyl <emily.lukas@gmail.com>
2021-01-02 22:52:10 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="content">
+        <div id="web-header">
+          <span id="name-tag">emily lukas</span>
+          <div id="link-header">
+            <span className="top-link">about</span>
+            <span className="top-link">projects</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
