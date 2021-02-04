41a905d682027b576f02bc60855b07b1619bb901
emilyl <emily.lukas@gmail.com>
2021-02-03 18:58:29 -0800

    styling updates + start projects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import { useEffect } from 'react';
-import anime from 'animejs/lib/anime.es.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import IntroPic from './components/IntroPic/IntroPic.js';
 import { ReactComponent as EmailIcon } from './assets/EmailIcon.svg';
 import { ReactComponent as GithubIcon } from './assets/GithubIcon.svg';
 import { ReactComponent as LinkedinIcon } from './assets/LinkedinIcon.svg';
+import { ReactComponent as PilldexPhone } from './assets/pilldexMockup.svg';
 
 function App() {
 
   useEffect(() => {
+    AOS.init();
+    AOS.refresh();
     const script = document.createElement('script');
 
     script.src = "https://code.iconify.design/1/1.0.6/iconify.min.js";
@@ -68,39 +73,40 @@ function App() {
           </div>
         </div>
 
-        <p className="section-header">about -</p>
-        <div className="row-content">
+        <p className="section-header" id="about" data-aos="fade-up" data-aos-duration="800">about -</p>
+        <div className="row-content" data-aos="fade-up" data-aos-duration="1000">
           <div className="intro-paragraph">
-            <p>Hey! I’m Emily and I’m currently in my 3rd year of Computer Engineering at UBC.
+            <p>Hey! I’m Emily and I’m currently in my 3rd year of Computer Engineering at the University of British Columbia.
+              Currently, most of my experience is in <mark className="highlight">front-end development with a
+              touch of full-stack.</mark>
+            <br></br>
+            <br></br>
             I’m passionate about projects that promote <mark className="highlight">wellness,
-            accessibility, and sustainability,</mark> as these topics are so important in shaping our future.
-            Currently, most of my experience is in <mark className="highlight">front-end development with a
-            touch of full-stack.</mark> When I’m not on my laptop coding, you’ll
+            accessibility, and sustainability,</mark> as these topics are so important in shaping our future!
+            When I’m not on my laptop coding, you’ll
             likely find me with a cup of matcha scouting out new food places to try:)</p>
           </div>
           <div className="comment-box">
             <p className="comment-line">/*</p>
             <p className="comment-line">&nbsp;* <span className="comment-label">Author:</span> Emily Lukas</p>
-            <p className="comment-line">&nbsp;* <span className="comment-label">Interests:</span> software dev, tech in medicine</p>
+            <p className="comment-line">&nbsp;* <span className="comment-label">Interests:</span> mobile + web dev, tech in medicine</p>
             <p className="comment-line">&nbsp;* <span className="comment-label">Hobbies:</span>  food, friends, and tea</p>
             <p className="comment-line">&nbsp;* <span className="comment-label">Favorite Figma Plugin:</span> Blobs</p>
             <p className="comment-line">&nbsp;*/</p>
           </div>
         </div>
 
-        <p className="section-header">education -</p>
-        <div className="education-content">
-          <p>The University of British Columbia</p>
-        </div>
-
-        <p className="section-header">selected projects -</p>
+        <p className="section-header" data-aos="fade-up" data-aos-duration="800">selected projects -</p>
         <div className="project-container">
-          <div className="row-content">
-            <div className="column-content">
+          <div className="project">
+            <div className="project-pic">
+              <PilldexPhone style={{height: '100%', width: '100%'}}/>
+            </div>
+            <div className="project-description">
               <h2 className="project-name">pilldex</h2>
               <p>a pillbox app that parses images of prescription labels and fosters medication adherence</p>
               <p>technologies-</p>
-              <p>React Native, Node,js, MongoDB, Firebase, TravisCI, Detox/Jest, Figma</p>
+              <p>React Native, Node.js, MongoDB, Firebase, Detox/Jest, Figma</p>
             </div>
           </div>
         </div>
